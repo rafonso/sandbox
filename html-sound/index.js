0f4232b49ff2750422e03fa0520cad1d9af5064f
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2019-08-23 19:07:17 -0300

    Obj para controle do audio; Prep para intervalo

--- html-sound/index.js
+++ html-sound/index.js
@@ -3,80 +3,152 @@
     "use strict";
 })();
 
-let audioContext;
-let oscillator;
-let gain;
+let audio = {
+    _audioContext: null,
+    _oscillator: null,
+    _gain: null,
+    _running: false,
+    start: function (volume, type, frequency) {
+        console.log("Iniciando");
+        this._audioContext = new AudioContext();
+        this._oscillator = this._audioContext.createOscillator();
+        this._gain = this._audioContext.createGain();
+
+        this._gain.gain.value = volume;
+        this._oscillator.type = type;
+        this._oscillator.frequency.value = frequency;
+        this._oscillator.connect(this._gain);
+        this._gain.connect(this._audioContext.destination);
+        this._oscillator.start(0);
+
+        this._running = true;
+        console.log("Iniciando");
+    },
+    stop: function () {
+        console.log("Parando");
+        this._oscillator.stop();
+        if (this._audioContext.close) { // MS has not context.close
+            this._audioContext.close();
+        }
+
+        this._gain = null;
+        this._oscillator = null;
+        this._audioContext = null;
+        this._running = false;
+        console.log("Parado");
+    },
+    get type() {
+        return this._oscillator.type;
+    },
+    set type(type) {
+        this._oscillator.type = type;
+    },
+    get frequency() {
+        return this._oscillator.frequency.value;
+    },
+    set frequency(frequency) {
+        this._oscillator.frequency.value = frequency;
+    },
+    get volume() {
+        return this._gain.gain.value;
+    },
+    set volume(volume) {
+        this._gain.gain.value = volume;
+    },
+    get running() {
+        return this._running;
+    }
+};
+
 
 function iniciarSom() {
-    audioContext = new AudioContext();
-    oscillator = audioContext.createOscillator();
-    gain = audioContext.createGain();
-
-    gain.gain.value = $("#gain").val();
-    oscillator.type = $("#type").val();
-    oscillator.frequency.value = $("#frequency").val();
-    oscillator.connect(gain);
-    gain.connect(audioContext.destination);
-    oscillator.start(0);
+    audio.start($("#gain").val(), $("#type").val(), $("#frequency").val());
 
     $("#btSound").text("Parar");
-    console.debug(audioContext);
+    $("#btnExibeIntervalos").prop("disabled", true);
 }
 
 function pararSom() {
-    oscillator.stop();
-    if (audioContext.close) { // MS has not context.close
-        audioContext.close();
-    }
-    audioContext = null;
-    oscillator = null;
-    gain = null;
+    audio.stop();
 
     $("#btSound").text("Iniciar");
-    console.debug(audioContext);
+    $("#btnExibeIntervalos").prop("disabled", false);
 }
 
+/**
+ * 
+ * @param {JQuery} control 
+ * @param {Event} event 
+ * @param {number} decimais 
+ */
+function normalizarValor(event, decimais) {
 
-$(document).ready(function () {
-    if (!AudioContext) {
-        console.error("Não tem Audio!");
-        $("form :input").prop("disabled", true);
-        return;
+    function valorInteiro(ctrl, delta, sentido) {
+        let newVal = parseInt(ctrl.val()) + sentido * delta;
+        return (newVal > 0) ? newVal : 0;
     }
 
-    console.info("Audio Pronto!");
+    function valorDecimal(ctrl, delta, sentido) {
+        let newVal = (Math.round((parseFloat(ctrl.val()) + sentido * delta) * 100) / 100).toFixed(decimais);
 
-    $("#btSound").click(() => !!audioContext ? pararSom() : iniciarSom());
-    $("#type").on('change', () => {
-        if (!!audioContext) {
-            oscillator.type = $("#type").val();
+        if (newVal > parseFloat(ctrl.prop("max"))) {
+            return ctrl.prop("max");
+        } else if (newVal < 0) {
+            return "0." + "0".repeat(decimais);
+        } else if (Number.isInteger(newVal)) {
+            return newVal + "." + "0".repeat(decimais);
         }
-    });
+        return newVal;
+    }
+
+    let control = $(event.currentTarget);
+    let step = decimais ? parseFloat(control.prop("step")) : parseInt(control.prop("step"));
+    let delta = (event.ctrlKey ? 10 : 1) * step;
+    let sentido = event.deltaY;
+    let conversor = decimais ? valorDecimal : valorInteiro;
+    let newVal = conversor(control, delta, sentido);
+    console.debug(newVal);
+
+    control.val(newVal).trigger("change");
+    event.preventDefault();
+}
+
+function init() {
+    $("#btSound").click(() => audio.running ? pararSom() : iniciarSom());
+    $("#type").on('change', (event) => {
+        if (audio.running) {
+            audio.type = $(event.currentTarget).val();
+        }
+    }).focus();
     $("#frequency").on('change', (event) => {
-        if (!!audioContext) {
-            oscillator.frequency.value = $("#frequency").val();
+        if (audio.running) {
+            audio.frequency = $(event.currentTarget).val();
         }
-    }).on('mousewheel', function (event) {
-        let delta = event.ctrlKey ? 100 : 10;
-        let newVal = parseInt($("#frequency").val()) + event.deltaY * delta;
-        newVal = (newVal > 0) ? newVal : 0;
-        $("#frequency").val(newVal).trigger("change");
-        event.preventDefault();
-    });
+    }).on('mousewheel', event => normalizarValor(event, 0));
     $("#gain").on('change', (event) => {
-        if (!!audioContext) {
-            gain.gain.value = $("#gain").val();
+        if (audio.running) {
+            audio.volume = $(event.currentTarget).val();
         }
-    }).on('mousewheel', function (event) {
-        let delta = event.ctrlKey ? 0.10 : 0.01;
-        let newVal = Math.round((parseFloat($("#gain").val()) + event.deltaY * delta) * 100) / 100;
-        newVal = (newVal > 0) ?
-            (/\d*\.\d\d$/.test(newVal) ? newVal : (newVal + '0')) :
-            '0.00';
-        $("#gain").val(newVal).trigger("change");
-        event.preventDefault();
-    });
-    $("#type").focus();
-});
+    }).on('mousewheel', event => normalizarValor(event, 2));
+    $("#tempoAtivo").on('change', (event) => {
+        if (audio.running) {
+            console.debug(event.currentTarget.id, $(event.currentTarget).val());
+        }
+    }).on('mousewheel', event => normalizarValor(event, 1));
+    $("#tempoInativo").on('change', (event) => {
+        if (audio.running) {
+            console.debug(event.currentTarget.id, $(event.currentTarget).val());
+        }
+    }).on('mousewheel', event => normalizarValor(event, 1));
 
-// (Math.round($("#gain").val() * 100) / 100) + event.deltaY * delta
+    console.info("Audio Pronto!");
+}
+
+$(document).ready(function () {
+    if (!!AudioContext) {
+        init();
+    } else {
+        $("form :input").prop("disabled", true);
+        console.error("Não tem Audio!");
+    }
+});
